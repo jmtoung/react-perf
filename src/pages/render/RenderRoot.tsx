52ee08c18811bb10bdd118f6e32d3160ba5db42a
Jonathan Toung <jonathan.toung@robinhood.com>
2024-12-31 10:31:19 -0800

    Add color box

--- src/pages/render/RenderRoot.tsx
+++ src/pages/render/RenderRoot.tsx
@@ -1,5 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const COLORS = ['black', 'red', 'orange', 'yellow', 'green', 'blue']
 
 export function RenderRoot() {
-    return <div>"Render Root"</div>
+    const [colorIndex, setColorIndex] = useState(0)
+
+    return (
+        <>
+            <button onClick={() => { setColorIndex((colorIndex + 1) % COLORS.length)}}>"Change the color of the box"</button>
+            <ColorBox color={COLORS[colorIndex]} />
+        </>)
+}
+
+function ColorBox({color}: {color: string}) {
+    return (
+        <div style={{ height: 100, width: 100, backgroundColor: color }} />
+    )
 }
